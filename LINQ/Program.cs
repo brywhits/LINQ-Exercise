ce12b8a9fcc62bf1b0206c581427efc7e68471ec
bryantwhitsett <bryantwhitsett71@gmail.com>
2023-06-23 11:55:05 -0400

    LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace LINQ
+﻿using System.Linq;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var videoGameNames = new List<string>()
+            {
+                "Zelda",
+                "Mario",
+                "Avengers",
+                "SpiderMan",
+                "Batman",
+                "Joker"
+            };
+
+            var gameList = videoGameNames.OrderBy(game => game.Length).ToList();
+
+            gameList.ForEach(game => Console.WriteLine(game));
         }
     }
 }
